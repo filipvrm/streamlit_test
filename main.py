881cbb676844767d40dbcd3d5ab62dc680064746
Filip von Reis Marlevi <filipvrm@Filips-MacBook-Pro.local>
2024-11-05 15:58:39 +0100

    personalized title

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-st.title("Simple Data Dashboard")
+st.title("Filips Simple Data Dashboard")
 
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
